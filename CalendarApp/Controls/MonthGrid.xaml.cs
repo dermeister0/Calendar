c554462d65d9ab0930b7f616266f174328f94a06
Der_Meister <der@heavysoft.ru>
2014-01-15 21:37:51 +0800

    Create day cells.

--- CalendarApp/Controls/MonthGrid.xaml.cs
+++ CalendarApp/Controls/MonthGrid.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using CalendarApp.Utils;
 
 namespace CalendarApp.Controls
 {
@@ -15,6 +16,22 @@ namespace CalendarApp.Controls
         public MonthGrid()
         {
             InitializeComponent();
+
+            for (int w = 0; w < CalendarSupport.WeeksInMonth; w++)
+            {
+                for (int d = 0; d < CalendarSupport.DaysInWeek; d++)
+                {
+                    var cell = new DayCell();
+                    Grid.SetRow(cell, w + 1);
+                    Grid.SetColumn(cell, d);
+
+                    MonthCellGrid.Children.Add(cell);
+                }
+            }
+        }
+
+        void CreateCells()
+        {
         }
     }
 }
---
+++ CalendarApp/Utils/CalendarSupport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalendarApp.Utils
+{
+    class CalendarSupport
+    {
+        public const int DaysInWeek = 7;
+
+        public const int WeeksInMonth = 5;
+    }
+}
